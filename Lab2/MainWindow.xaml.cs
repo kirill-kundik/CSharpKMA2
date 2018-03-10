a5c8e6c1d2ecd3e1817e35c8ad19ea01ec31699a
kirill-kundik <goliathusua@gmail.com>
2018-03-10 21:05:37 +0200

    sorry i've got my laptop broken and high temperature during TSR and only now i have a chance to publish my homework , i'm very sorry

---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new SignUpViewModel(ShowResultView);
+        }
+
+        private void ShowResultView()
+        {
+            SignUpViewModel data = DataContext as SignUpViewModel;
+            if (data != null)
+            {
+                Person user = new Person(data.FirstName, data.LastName, data.Email, data.DateOfBirth);
+                string bday = user.IsBirthday ? "Happy Birthday!!!" : "";
+                MessageBox.Show(
+                    $"First name: {user.FirstName}\n" +
+                    $"Last name: {user.LastName}\n" +
+                    $"Email: {user.Email}\n" +
+                    $"Date of birth: {user.DateOfBirth}\n" +
+                    $"Adult: {user.IsAdult}\n" +
+                    $"Our Sign: {user.SunSign}\n" +
+                    $"Chinese Sign: {user.ChineseSign}\n" +
+                    $"{bday}"
+                );
+            }
+        }
+    }
+}
